8bb163143c0e064929a9ea3472e0d6a8fd7987b0
Game <thapat.g11@gmail.com>
2023-09-24 18:11:42 +0700

    Added Nightly and words.txt

--- WordCheck-Nightly.py
+++ WordCheck-Nightly.py
@@ -1,1 +1,37 @@
+# WordCheck
+# By Noigamegun
+# Version Nightly
+# words.txt is required
+
+start_confirm = str(input("Loading the words maybe freeze your PC temporary. Continue? [y,n] : ")).strip().lower()
+if start_confirm == "y":
+    print("\n")
+    print("Loading words list...")
+    file_path = "words.txt"
+    word_list = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            word_list.append(line.strip())
+
+    print("\n")
+    print("Loading Done.")
+
+    while True:
+        print("\n")
+        word_to_check = input("Enter a word (Enter \"stop the program\" to stop): ").strip().lower()
+        if word_to_check == "stop the program":
+            print("\n")
+            print("Stopped")
+            break
+        else:
+            print("\n")
+            if word_to_check in word_list:
+                print(f"The word \"{word_to_check}\" is a word.")
+            else:
+                print(f"The word \"{word_to_check}\" is not a word.")
+
+else:
+    print("Aborted")
+
 
