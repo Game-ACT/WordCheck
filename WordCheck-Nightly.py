2c7f63b622eb0dc161b425689a3899543b0ef861
noigamegun <76461912+noigamegun@users.noreply.github.com>
2023-10-27 12:43:13 +0700

    fixed some stuff

--- WordCheck-Nightly.py
+++ WordCheck-Nightly.py
@@ -7,14 +7,14 @@ start_confirm = str(input("Loading the words maybe freeze your PC temporary. Con
 if start_confirm == "y":
     while True:
         try:
-            file_path = str(input("Enter file name (Must be a txt file.  Not Case-sensitive. The program will remove and starting and ending spaces.)"))
+            file_path = str(input("Enter file name (Must be a txt file.  Case-sensitive. The program will adjust the list to be compatible.)"))
             print("\n")
             print("Loading words list...")
             word_list = []
 
             with open(file_path, "r") as file:
                 for line in file:
-                    word_list.append(line.strip())
+                    word_list.append(line.strip().lower())
 
             print("\n")
             print("Loading Done.")
